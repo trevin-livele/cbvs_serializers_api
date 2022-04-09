c4eb9259badf4ff2bd86c48364474e695772cfe8
trevin-livele <liveletrevin6@gmail.com>
2022-04-09 21:29:50 +0300

    added class student detail view

--- cbvApp/views.py
+++ cbvApp/views.py
@@ -4,6 +4,7 @@ from cbvApp.serializers import StudentSerializer
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
+from django.http import HTTP404
 
 
 # Create your views here.
@@ -21,4 +22,26 @@ class StudentListView(APIView):
             serializer.save()
             return Response(serializer.data,status=status.HTTP_201_CREATED)
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+
+class StudentDetailView(APIView):
+    def get_object(self, pk):
+        try:
+            return Student.objects.get(pk=pk)
+        except Student.DoesNotExist:
+            raise HTTP404
+
+    def get(self, request, pk):
+        student = self.get_object(pk)
+        serializer = StudentSerializer(student,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        student = self.get_object(pk)
+        student.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 

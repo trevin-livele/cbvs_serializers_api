156646b2f0785e383b1741a724f94d42ba458674
trevin-livele <liveletrevin6@gmail.com>
2022-04-09 21:20:19 +0300

    added student list api view

--- cbvApp/views.py
+++ cbvApp/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from cbvApp.models import Student
+from cbvApp.serializers import StudentSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+
 
 # Create your views here.
+
+class StudentListView(APIView):
+
+    def get(self, request):
+        students = Student.objects.all()
+        serializer = StudentSerializer(students, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
